70131599bebb8033274dddaac4561e0415106fe4
lukaszm-rc <lukasz.mazurek@redcart.pl>
2016-01-14 14:06:39 +0100

    fix + optional bootstrap package.json

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 export class App {
-  message = 'Welcome to Aurelia!'; 
-  constructor() {
-}
+	message = "Welcome to Aurelia";
+  configureRouter(config, router) {
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
+      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
+      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
+    ]);
+
+    this.router = router;
+  }
 }
